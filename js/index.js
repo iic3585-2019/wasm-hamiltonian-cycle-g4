8f9741d56f9cee1aa0a4798202d8c5e64e046f52
Mauricio Ortiz <maortiz4@uc.cl>
2019-04-15 04:36:54 -0400

    a bit of order

--- js/index.js
+++ js/index.js
@@ -8,12 +8,37 @@ import("../crate/pkg").then(module => {
       (3)-------(4)
   */
   const solveButton = document.getElementById("solve");
+  const updateButton = document.getElementById("update");
 
   let svg = d3.select("svg"),
   width = +svg.attr("width"),
   height = +svg.attr("height");
   
   solveButton.addEventListener("click", _ => {
+    const nodes = parseInt(document.getElementById("nodes").value, 10);
+    const edges = document.getElementById("edges").value;
+
+    const result =  module.search_hamiltonian(nodes, edges);
+    let output = document.getElementById("result");
+
+
+    let array = [];
+    if(result[0] != -1) {
+      for (let i = 0; i < result.length-1; i++) {
+        array.push({source: result[i], target: result[i+1]});
+      }
+
+      
+      
+      let s = result.reduce((a, b) => a+' -> '+b);
+      output.innerHTML = s+' -> '+result[0];
+    } else {
+      output.innerHTML = "No tiene camino hamiltoneano";
+    }
+
+
+  })
+  updateButton.addEventListener("click", _ => {
     d3.selectAll("svg > *").remove();
     const nodes = parseInt(document.getElementById("nodes").value, 10);
     let nodes_data = [...Array(nodes).keys()].map((x) => { return {id: x.toString()} });
@@ -62,8 +87,6 @@ import("../crate/pkg").then(module => {
 
     drag_handler(node);
 
-    
-
     var link_force =  d3.forceLink(edges_data).id(function(d) { return d.id; }).distance(100)
     simulation.force("links",link_force)
 
@@ -79,18 +102,11 @@ import("../crate/pkg").then(module => {
     simulation.on("tick", tickActions );
 
     function tickActions() {
-        //update circle positions each tick of the simulation 
-        // node
-        //     .attr("cx", function(d) { return d.x; })
-        //     .attr("cy", function(d) { return d.y; });
         node
         .attr("transform", function(d) {
           return "translate(" + Math.max(7, Math.min(width - 7, d.x)) + "," + Math.max(7, Math.min(height - 7, d.y)) + ")";
         })
-            
-        //update link positions 
-        //simply tells one end of the line to follow one node around
-        //and the other end of the line to follow the other node around
+
         link
             .attr("x1", function(d) { return d.source.x; })
             .attr("y1", function(d) { return d.source.y; })
@@ -99,32 +115,21 @@ import("../crate/pkg").then(module => {
      
       } 
 
-      function drag_start(d) {
-        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-          d.fx = d.x;
-          d.fy = d.y;
-       }
+    function drag_start(d) {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+      }
        
-       function drag_drag(d) {
-         d.fx = d3.event.x;
-         d.fy = d3.event.y;
-       }
+    function drag_drag(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
        
-       
-       function drag_end(d) {
-         if (!d3.event.active) simulation.alphaTarget(0);
-         d.fx = null;
-         d.fy = null;
-       }
-    
-
-    const result =  module.search_hamiltonian(nodes, edges);
-    let output = document.getElementById("result");
-    if(result[0] != -1) {
-      let s = result.reduce((a, b) => a+' -> '+b);
-      output.innerHTML = s+' -> '+result[0];
-    } else {
-      output.innerHTML = "No tiene camino hamiltoneano";
+    function drag_end(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
     }
   });
 });
